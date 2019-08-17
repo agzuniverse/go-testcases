8b61fe7ff34c59d364f0d865c724600f22780e81
Aswin G <aswinganesh666@gmail.com>
2019-08-17 21:07:13 +0530

    return error if n<=1 for multirandom generation

--- src/components/genMultiRandom.go
+++ src/components/genMultiRandom.go
@@ -1,7 +1,12 @@
 package components
 
+import "errors"
+
 // GenMultiRandom generates n random numbers between min and max.
 func GenMultiRandom(n int, min int, max int) ([]int, error) {
+	if n <= 0 {
+		return nil, errors.New("n must be at least 1")
+	}
 	result := make([]int, 0)
 	for i := 0; i < n; i++ {
 		v, err := GenSingleRandom(min, max)
