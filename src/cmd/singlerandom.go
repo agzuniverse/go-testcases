200c58334a35e31d0987a30a67ff51fff3f4f728
Aswin G <aswinganesh666@gmail.com>
2019-08-17 14:07:40 +0530

    Created generate single random number functionality

--- src/cmd/singlerandom.go
+++ src/cmd/singlerandom.go
@@ -1,1 +1,32 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/agzuniverse/go-testcases/src/utils"
+
+	"github.com/spf13/cobra"
+)
+
+var singleRandom = &cobra.Command{
+	Use:   "singlerandom",
+	Short: "Generate a single random positive integer between MIN and MAX",
+	Run: func(cmd *cobra.Command, args []string) {
+		v, err := utils.GenSingleRandom(min, max)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+		fmt.Println(v)
+	},
+}
+
+var min int
+var max int
+
+func init() {
+	singleRandom.Flags().IntVar(&min, "min", 0, "Minimum value of random number")
+	singleRandom.Flags().IntVar(&max, "max", 0, "Maximum value of random number")
+	rootCmd.AddCommand(singleRandom)
+}
